89e1173642bf60b0563c12e325daa38aed8d7fa9
MaxOniHub <onyshchenko.max@gmail.com>
2018-07-01 18:43:59 +0300

    - added `CustomerDataFetcherService`

---
+++ app/Services/CustomerDataFetcherService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services;
+
+use App\User;
+
+class CustomerDataFetcherService
+{
+    /**
+     * @var User
+     */
+    private $repository;
+
+    public function setRepository(User $user)
+    {
+        $this->repository = $user;
+    }
+
+    public function getUsersTransactions()
+    {
+        return $this->repository->transactions()->active()->get();
+    }
+
+    public function getUsersTransaction($transaction_id)
+    {
+        return $this->repository->transactions()->active()->where('id', $transaction_id)->get();
+    }
+}
